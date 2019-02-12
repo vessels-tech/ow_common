37113d6a6db377ea41f0f8087d4295da49c8a474
Lewis Daly <lewisdaly@me.com>
2019-02-12 18:52:13 +0800

    round in circles we go

--- lib/model/dep_ShortIdDoc.d.ts
+++
@@ -1,9 +0,0 @@
-import { FirestoreDoc } from "../utils/FirestoreDoc";
-import DocNames from "../enums/DocNames";
-import { ShortId } from './ShortId';
-import { Firestore } from "@google-cloud/firestore";
-export declare class ShortIdDoc extends FirestoreDoc<ShortId> {
-    static docName: DocNames;
-    static get<ShortId>(firestore: Firestore, orgId: string, id: string): Promise<FirestoreDoc<ShortId>>;
-    static parse<U>(data: any): U;
-}
